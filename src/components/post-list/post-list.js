749649ec8c0f74cbce0dab8502b1c500142c0065
manager225 <manager225@mail.ru>
2020-08-27 16:56:07 +0300

    one_commit

---
+++ src/components/post-list/post-list.js
@@ -0,0 +1,34 @@
+import React from "react";
+import PostListItem from "../post-list-item";
+import './post-list.css'
+
+const PostList = ({posts, onDelete, onToggleImportant, onToggleLiked}) =>{
+
+    const elements = posts.map((item) =>{
+        const {id, ...itemProps} = item;
+        return (
+            <li key={id} className="list-group-item">
+                <PostListItem
+                    {...itemProps}
+                    onDelete={() => onDelete(id)}
+                    onToggleImportant = {() =>{
+                        onToggleImportant(id)
+                    }}
+                    onToggleLiked = {() =>{
+                        onToggleLiked(id)
+                    }}
+                // label={item.label}
+                // important={item.important}
+                />
+            </li>
+        )
+    })
+
+    return (
+        <ul className="app-list list-group">
+            {elements}
+        </ul>
+    )
+}
+
+export default PostList;
